7cc2c8cdc012548f80e2c46b0bc5296e0e0d0c83
Tatsuro Osuka <oscarwild13@gmail.com>
2022-04-30 18:34:11 +0900

    class-validator

--- src/items/dto/create-item.tdo.ts
+++ src/items/dto/create-item.tdo.ts
@@ -1,6 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsString, IsNotEmpty, MaxLength, IsInt, Min } from 'class-validator';
+
 export class CreateItemDto {
-  id: string;
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
